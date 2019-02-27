82fd5af24a1539fad64f3dc93c2ee799b6220312
Matias De Santi <matias.desanti@wolox.com.ar>
2019-02-27 15:57:39 -0300

    Pull request fixes

--- src/main/java/wolox/training/TrainingApplication.java
+++ src/main/java/wolox/training/TrainingApplication.java
@@ -2,9 +2,7 @@ package wolox.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("wolox.training.repositories")
 @SpringBootApplication
 public class TrainingApplication {
 
